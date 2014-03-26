4451828ebf92f53c50d45a1c34690ab525769d94
Artur Wróbel <arturwrobel@home.pl>
2014-03-26 13:51:41 +0100

    extend syncano with PubSub mixin

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@
 
 // Base function.
 var syncano = function() {
-  // Add functionality here.
-  return true;
+	// Add functionality here.
+	return true;
 };
 
+syncano.prototype = extend(syncano.prototype, PubSub);
+
+syncano.prototype.test = function(){
+	console.log('test', this.hasSubscribers('message'));
+};
 
 // Version.
-syncano.VERSION = '0.0.0';
+syncano.VERSION = '0.0.1';
 
 
 // Export to the root, which is probably `window`.
