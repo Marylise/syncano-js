a146dcf443e8e0ffd6b924dbc8f51527bfd9ff24
Artur Wróbel <arturwrobel@home.pl>
2014-03-31 13:33:55 +0200

    Folders - getOne, update, delete

--- dist/syncano.js
+++ dist/syncano.js
@@ -648,6 +648,98 @@ Folder.get = function(projectId, collection, callback){
 	this.__super__.__sendWithCallback(method, params, 'folder', callback);
 };
 
+
+/**
+ *  Get folders for specified collection 
+ * 
+ *  @name method Folder.getOne 
+ *  @param {number} projectId Project id that collection will be created for
+ *  @param {string / Number} collection Collection id or key defining collection for which folder will be returned
+ *  @param {string} folderName Folder name defining folder
+ *  @param {function} callback (optional) Function to be called when successful response comes
+ */
+Folder.getOne = function(projectId, collection, folderName, callback){
+	this.__super__.__checkProjectId(projectId);
+	
+	var method = 'folder.get_one';
+	var params = {
+		project_id: projectId
+	};
+	params = this.__super__.__addCollectionIdentifier(params, collection);
+	
+	if(typeof folderName !== 'string'){
+		throw new Error('FolderName must be a string');
+	}
+	params.folder_name = folderName;
+	this.__super__.__sendWithCallback(method, params, 'folder', callback);
+};
+
+
+/**
+ *  Update existing folder
+ * 
+ *  @method Folder.update
+ *  @param {number} projectId Project id that collection will be created for
+ *  @param {string / Number} collection Collection id or key defining collection for which folder will be returned
+ *  @param {string} folderName Folder name defining folder
+ *  @param {string} newName New folder name
+ *  @sourceId {string} New source id, can be used for mapping folders to external source
+ *  @param {function} callback (optional) Function to be called when successful response comes
+ */
+Folder.update = function(projectId, collection, folderName, newName, sourceId, callback){
+	this.__super__.__checkProjectId(projectId);
+	var method = 'folder.update';
+	var params = {
+		project_id: projectId
+	};
+	params = this.__super__.__addCollectionIdentifier(params, collection);
+	
+	if(typeof folderName !== 'string'){
+		throw new Error('FolderName must be a string');
+	}
+	params.name = folderName;
+	
+	if(typeof newName !== 'undefined' && newName !== null){
+		if(typeof newName !== 'string'){
+			throw new Error('newName must be a string');
+		}
+		params.new_name = newName;
+	} else {
+		throw new Error('newName must be passed');
+	}
+	
+	if(sourceId !== null){
+		params.source_id = sourceId + '';
+	}
+	this.__super__.__sendWithCallback(method, params, 'folder', callback);
+};
+
+
+/**
+ *  Permanently delete specified folder and all associated data
+ *
+ *  @method Folder.delete
+ *  @param {number} projectId Project id that collection will be created for
+ *  @param {string / Number} collection Collection id or key defining collection for which folder will be returned
+ *  @param {string} folderName Folder name defining folder
+ *  @param {function} callback (optional) Function to be called when successful response comes
+ */
+Folder.delete = function(projectId, collection, folderName, callback){
+	this.__super__.__checkProjectId(projectId);
+	var method = 'folder.delete';
+	var params = {
+		project_id: projectId
+	};
+	params = this.__super__.__addCollectionIdentifier(params, collection);
+	
+	if(typeof folderName !== 'string'){
+		throw new Error('FolderName must be a string');
+	}
+	params.name = folderName;
+	
+	this.__super__.__sendWithCallback(method, params, null, callback);
+};
+
 /**
  *  
  */
