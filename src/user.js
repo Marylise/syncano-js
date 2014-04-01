2476051e6c7aba9dbb437e764eecb0d9b87c1096
Artur Wróbel <arturwrobel@home.pl>
2014-04-01 16:06:31 +0200

    documentation generated by YUIdoc

--- src/user.js
+++ src/user.js
@@ -62,3 +62,50 @@ User.getAll = function(sinceId, limit, callback){
 	
 	this.__super__.__sendWithCallback(method, params, 'user', callback);
 };
+
+
+/**
+ *  Get users of specified criteria that are associated with Data Objects within specified collection 
+ *
+ *  @method Data.count
+ *  @param {number} projectId Project id
+ *  @param {string / Number} collection Either collection id or key
+ *  @param {object} [optionalParams] Optional parameters:
+ *  @param {string} [optionalParams.state] Return only users whose Data Objects are in specified state. Accepted values: Pending, Moderated, Rejected, All. Default value: All
+ *  @param {string / Array} [optionalParams.folders] Folder name that data will be returned from. Max 100 values per request. If not present returns data from across all collection folders
+ *  @param {string} [optionalParams.filter] TEXT - only return users that sent data with text IMAGE - only return users that sent data with an image
+ *  @param {function} [callback] Function to be called when successful response comes
+ */
+User.get = function(projectId, collection, optionalParams, callback){
+	this.__super__.__checkProjectId(projectId);
+
+	var method = 'user.get';
+	var params = {
+		project_id: projectId
+	};
+	params = this.__super__.__addCollectionIdentifier(params, collection);
+
+	if(isset(optionalParams)){
+		if(isset(optionalParams.folders)){
+			params.folders = optionalParams.folders;
+		}
+		
+		if(isset(optionalParams.state)){
+			if(inArray(optionalParams.state.toLowerCase(), ['pending','moderated','rejected','all'])){
+				params.state = optionalParams.state;
+			} else {
+				throw new Error('incorrect value of state param');
+			}
+		}
+		
+		if(isset(optionalParams.filter)){
+			if(inArray(optionalParams.filter.toLowerCase(), ['text', 'image'])){
+				params.filter = optionalParams.filter;
+			} else {
+				throw new Error('incorrect value of filter param - only "text" and "image" are allowed');
+			}
+		}
+	}
+	
+	this.__super__.__sendWithCallback(method, params, 'user', callback);
+};
